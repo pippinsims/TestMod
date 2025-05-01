de505424dad1aebfd5ebace3452564fc0a94526e
Sam <kuhnssamuel@gmail.com>
2025-05-01 12:13:08 -0400

    Add example_item.json and example_item_2.json

--- src/main/java/dev/zordin/testmod/TestMod.java
+++ src/main/java/dev/zordin/testmod/TestMod.java
@@ -1,8 +1,18 @@
 package dev.zordin.testmod;
 
+import dev.zordin.testmod.Init.ItemInit;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(TestMod.MODID)
 public class TestMod {
     public static final String MODID = "testmod";
+
+    public TestMod()
+    {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ItemInit.ITEMS.register(bus);
+    }
 }
